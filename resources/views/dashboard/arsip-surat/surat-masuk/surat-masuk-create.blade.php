1262d3cc3501aee43bf7542be0fe4943e128ca3c
esaich <saidhamzah12@gmail.com>
2023-08-19 16:37:14 +0700

    memperbaiki arsip surat masuk

--- resources/views/dashboard/arsip-surat/surat-masuk/surat-masuk-create.blade.php
+++ resources/views/dashboard/arsip-surat/surat-masuk/surat-masuk-create.blade.php
@@ -1,5 +1,52 @@
 @extends('dashboard.layouts.template')
 
+<style>
+  .input-group{
+    position: relative;
+    margin: 20px 0;
+  }
+
+  .input-group label{
+    position: absolute;
+    top: 50%;
+    left:5px;
+    transform: translateY(-50%);
+    font-size:16px;
+    color:black;
+    padding: 0 5px;
+    pointer-events:none; 
+    transition: .5s;
+  }
+
+  .input-group input{
+    width: 320px;
+    height: 40px;
+    font-size: 16px;
+    padding: 0 10px;
+    background: transparent;
+    border: 1.2px solid rgb(91, 129, 255);
+    outline: none;
+    border-radius: 7px !important;
+  }
+  
+  .input-group input:focus~label{
+    font-size: 12px;
+    top: 0;
+    color: black;
+    background: #fff;
+  }
+
+
+  .input-group input:valid~label {
+    font-size: 12px;
+    top: 0;
+    color: black;
+    background: #fff;
+    
+  }
+
+</style>
+
 @section('content')
 
 <div class="card">
@@ -7,51 +54,51 @@
         <div class="card-body">
             <form action="{{ url('surat-masuk') }}" method="POST" enctype="multipart/form-data">
                 @csrf
-                <div class="mb-3">
-                  <label for="nomor_urut" class="form-label">Nomor Urut</label>
-                  <input type="text" class="form-control" id="nomor_urut" name="nomor_urut">
-                  <div class="form-text">We'll never share your email with anyone else.</div>
+                <div class="input-group">
+                  <input type="text" class="" id="nomor_urut" name="nomor_urut" required>
+                  <label for="nomor_urut" class="">Nomor Urut</label>
+                  
                 </div>
 
-                <div class="mb-3">
-                  <label for="tanggal_diterima" class="form-label">Tanggal Diterima</label>
-                  <input type="text" class="form-control" id="tanggal_diterima" name="tanggal_diterima">
-                  <div class="form-text">We'll never share your email with anyone else.</div>
+                <div class="input-group">
+                  <input type="text" id="tanggal_diterima" name="tanggal_diterima" required>
+                  <label for="tanggal_diterima" class="">Tanggal Diterima</label>
+                  
                 </div>
 
-                <div class="mb-3">
+                <div class="input-group">
+                  <input type="text" class="" id="nomor_surat" name="nomor_surat" required>
                   <label for="nomor_surat" class="form-label">Nomor Surat</label>
-                  <input type="text" class="form-control" id="nomor_surat" name="nomor_surat">
-                  <div class="form-text">We'll never share your email with anyone else.</div>
+                  
                 </div>
 
-                <div class="mb-3">
+                <div class="input-group">
+                  <input type="text" class="" id="tanggal_surat" name="tanggal_surat" required>
                   <label for="tanggal_surat" class="form-label">Tanggal Surat</label>
-                  <input type="text" class="form-control" id="tanggal_surat" name="tanggal_surat">
-                  <div class="form-text">We'll never share your email with anyone else.</div>
+                  
                 </div>
 
-                <div class="mb-3">
+                <div class="input-group">
+                  <input type="text" class="" id="perihal_surat" name="perihal_surat" required>
                   <label for="perihal_surat" class="form-label">Perihal Surat</label>
-                  <input type="text" class="form-control" id="perihal_surat" name="perihal_surat">
-                  <div class="form-text">We'll never share your email with anyone else.</div>
+                  
                 </div>
 
-                <div class="mb-3">
+                <div class="input-group">
+                  <input type="text" class="" id="asal_surat" name="asal_surat" required>
                   <label for="asal_surat" class="form-label">Asal Surat</label>
-                  <input type="text" class="form-control" id="asal_surat" name="asal_surat">
-                  <div class="form-text">We'll never share your email with anyone else.</div>
+                  
                 </div>
 
-                <div class="mb-3">
+                <div class="input-group">
+                  <input type="text" class="" id="isi_ringkas" name="isi_ringkas" required>
                   <label for="isi_ringkas" class="form-label">Isi Ringkas</label>
-                  <input type="text" class="form-control" id="isi_ringkas" name="isi_ringkas">
-                  <div class="form-text">We'll never share your email with anyone else.</div>
+                  
                 </div>
 
                 <div class="mb-3">
                   <label for="file_surat" class="form-label">File Surat</label>
-                  <input class="form-control" type="file" id="file_surat" name="file_surat">
+                  <input class="" type="file" id="file_surat" name="file_surat">
                   <div class="form-text">Upload File Surat</div>
                 </div>
                 
